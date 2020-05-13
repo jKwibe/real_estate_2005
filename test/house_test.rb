44dd9b8a67f8e45b068d969120f6aeb2eecaf692
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-13 09:33:06 -0600

    remove comments

--- test/house_test.rb
+++ test/house_test.rb
@@ -39,29 +39,3 @@ class HouseTest < Minitest::Test
     assert_instance_of Room, house.rooms[1]
   end
 end
-
-#
-# pry(main)> house = House.new("$400000", "123 sugar lane")
-# #=> #<House:0x00007fccd30375f8...>
-#
-# pry(main)> house.price
-# #=> 400000
-#
-# pry(main)> house.address
-# #=> "123 sugar lane"
-#
-# pry(main)> house.rooms
-# #=> []
-#
-# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
-# #=> #<Room:0x00007fccd29b5720...>
-#
-# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
-# #=> #<Room:0x00007fccd2985f48...>
-#
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
-# pry(main)> house.rooms
-# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
