6d15f86112dea7eaddb4d93875068a819abf594e
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-13 09:29:32 -0600

    test if it has rooms

---
+++ test/house_test.rb
@@ -0,0 +1,57 @@
+
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require "./lib/house"
+
+class HouseTest < Minitest::Test
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_instance_of House, house
+  end
+
+  def test_it_has_address_and_price
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_has_rooms
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_room
+
+  end
+end
+
+#
+# pry(main)> house = House.new("$400000", "123 sugar lane")
+# #=> #<House:0x00007fccd30375f8...>
+#
+# pry(main)> house.price
+# #=> 400000
+#
+# pry(main)> house.address
+# #=> "123 sugar lane"
+#
+# pry(main)> house.rooms
+# #=> []
+#
+# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
+# #=> #<Room:0x00007fccd29b5720...>
+#
+# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
+# #=> #<Room:0x00007fccd2985f48...>
+#
+# pry(main)> house.add_room(room_1)
+#
+# pry(main)> house.add_room(room_2)
+#
+# pry(main)> house.rooms
+# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
