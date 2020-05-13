c35a8568e599ae9467aaae9de3388117634ef34f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-13 09:12:35 -0600

    pass the area test

--- test/room_test.rb
+++ test/room_test.rb
@@ -9,16 +9,36 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
+    # skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
 end
+
+# pry(main)> require './lib/room'
+# #=> true
+#
+# pry(main)> room = Room.new(:bedroom, 10, "13")
+# #=> #<Room:0x00007fa53b9ca0a8 @category=:bedroom, @length=10, @width="13">
+#
+# pry(main)> room.category
+# #=> :bedroom
+#
+# pry(main)> room.area
+# #=> 130
+#
+# pry(main)> room.is_painted?
+# #=> false
+#
+# pry(main)> room.paint
+#
+# pry(main)> room.is_painted?
+# #=> true
